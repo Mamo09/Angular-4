bd6f25437440ab9842e7e849a93ca0f77d4fa6d2
Mamo09 <futmahidayat@gmail.com>
2022-07-01 09:31:29 +0700

    2 files modified

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -9,7 +9,7 @@ import { HttpHeaders, HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class StudentService {
-  private apiURL = environment.apiURL;
+  private apiURL = 'https://untitled-21zslm4grf6y.runkit.sh/api'
   
   httpOptions = {
     headers: new HttpHeaders({
